38e31ca0e008cfeb9669e1d5e5ec0f4a0962587b
kaery1000 <km4350@nyu.edu>
2019-01-20 20:44:32 -0500

    Initialized Project

---
+++ src/main/java/com/karan/joke/jokesapp/JokesappApplication.java
@@ -0,0 +1,14 @@
+package com.karan.joke.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesappApplication.class, args);
+    }
+
+}
+
